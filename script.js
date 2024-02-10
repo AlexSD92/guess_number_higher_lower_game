1cce388d9737857dcce0a080a54a1f9e5f332097
AlexSD92 <alex.spara.dominguez@gmail.com>
2024-02-10 10:58:01 +0000

    Create high scores array, push high scores when user guess is correct. Create function to toggle submit button, refactor reset button code out of continueGame and into addResetBtn

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let randomNumber;
 let testNumber = 13;
 let resetBtn = document.createElement("input");
 let submitBtn = document.querySelector("#submit");
+const highScores = [];
 const message = document.querySelector("#message");
 
 function getUserGuess() {
@@ -19,11 +20,22 @@ function clearMessage() {
   document.querySelector("#message").innerHTML = "";
 }
 
+function toggleSubmitBtn() {
+  if (submitBtn.style.display === "none") {
+    submitBtn.style.display = "block";
+  } else {
+    submitBtn.style.display = "none";
+  }
+}
+
 function compareNumbers() {
   getUserGuess();
   if (userGuess === randomNumber) {
     console.log("match");
     message.textContent = `you guessed the number with ${userAttempts} attempt(s) remaining! if you would like to play again, click reset`;
+    addResetBtn();
+    pushHighScore(userAttempts);
+    toggleSubmitBtn();
     document.querySelector("#reset").addEventListener("click", playAgain);
   } else if (userGuess < randomNumber) {
     message.textContent = `guess higher, ${userAttempts} attempt(s) remaining`;
@@ -39,21 +51,32 @@ function playAgain() {
   userAttempts = 5;
   randomNumber = generateRandomNumber();
   document.querySelector("#number").value = "";
+  document.querySelector("#reset").remove();
+  toggleSubmitBtn();
   console.log(randomNumber);
+}
+
+function addResetBtn() {
+  resetBtn.type = "submit";
+  resetBtn.value = "Reset";
+  resetBtn.id = "reset";
+  submitBtn.after(resetBtn);
 }
 
 function continueGame() {
   if (userAttempts > 0) {
     compareNumbers();
   } else {
-    resetBtn.type = "submit";
-    resetBtn.value = "Reset";
-    resetBtn.id = "reset";
-    submitBtn.after(resetBtn);
-
+    addResetBtn();
+    toggleSubmitBtn();
     message.textContent = `game over, ${userAttempts} attempt(s) remaining, if you would like to play again, click reset`;
     document.querySelector("#reset").addEventListener("click", playAgain);
   }
+}
+
+function pushHighScore(score) {
+  highScores.push(score);
+  console.log(highScores);
 }
 
 randomNumber = generateRandomNumber();
